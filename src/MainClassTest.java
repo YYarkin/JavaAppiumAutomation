0a229cb537f657befc2929fb44fc8b9980a328f4
YYarkin <Yarkin73@gmail.com>
2020-10-04 20:51:55 +0300

    second test

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,11 @@ public class MainClassTest extends MainClass {
         Assert.assertTrue(String.format("Ожидалось, что метод getLocalNumber вернет значение равное %s, получили %s",
                 expectedValue, this.getLocalNumber()), this.getLocalNumber() == expectedValue);
     }
+
+    @Test
+    public void testGetClassNumber() {
+        int borderValue = 45;
+        Assert.assertTrue(String.format("Ожидалось, что метод getClassNumber вернет значение больше чем %s, получили %s",
+                borderValue, this.getClassNumber()), this.getClassNumber() > borderValue);
+    }
 }
